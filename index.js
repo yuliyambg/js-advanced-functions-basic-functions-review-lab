569e534dfa6e5bb4ddb7ddb2a601416c31400986
yuliyambg <yuliyambg@gmail.com>
2021-02-19 16:47:14 -0500

    commit tree 4b38b0bd75628819de6d15f8d5ec485fca9f0097

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+function saturdayFun (activity="roller-skate") {
+return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(activity="go to the office") {
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(string="*") {
+    return function(par="special") {
+     return (`You are ${string}${par}${string}!`)
+    }
+}
+
+const Calculator = {
+    add: function(a,b) {
+        return a + b;
+    },
+    subtract: function(a,b) {
+        return a - b;
+    },
+    multiply: function(a,b) {
+        return a*b;
+    },
+
+    divide: function(a,b) {
+        return a / b;
+    }
+}
+
+function actionApplyer(start, arr) {
+ for (let i=0; i < arr.length; i++) {
+     start = arr[i](start);
+ }
+return start
+
+}
